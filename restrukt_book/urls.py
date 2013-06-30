fd3ec92ac58a73ac4568634790caea580ba8543c
DrMartiner <DrMartiner@GMail.Com>
2013-06-30 18:38:11 +0700

    Move recension to config

--- restrukt_book/urls.py
+++ restrukt_book/urls.py
@@ -4,23 +4,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
-from apps.simple_page.views import HomePage
-from apps.simple_page.views import MakeOrder
-from apps.simple_page.views import OrderFail
-from apps.simple_page.views import OrderSuccess
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', HomePage.as_view(), name='home_page'),
-    url(r'^make-order/$', MakeOrder.as_view(), name='order_make'),
-    url(r'^fail/$', OrderFail.as_view(), name='order_fail'),
-    url(r'^success/$', OrderSuccess.as_view(), name='order_success'),
+    url(r'^', include('apps.simple_page.urls')),
     url(r'^djangojs/', include('djangojs.urls')),
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^pay2pay/', include('pay2pay.urls')),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^sitemap\.xml', TemplateView.as_view(template_name='sitemao.xml', content_type='text/xml')),
-    url(r'^grappelli/', include('grappelli.urls')),
 )
 
 if settings.DEBUG:
