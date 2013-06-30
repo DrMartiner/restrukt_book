e7a3f2bfef6e33a80b39a997d52b59a2101c7c95
DrMartiner <DrMartiner@GMail.Com>
2013-06-30 22:17:29 +0700

    Casual admin

--- apps/simple_page/admin.py
+++ apps/simple_page/admin.py
@@ -13,7 +13,11 @@ class VideoAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('name', 'sent', 'address', 'email_html', 'created')
     list_filter = ('sent', 'created', 'payment__status')
+    list_editable = ('sent',)
     search_fields = ('name', 'address')
+    ordering = ('-sent', 'created')
+
+    readonly_fields = ('name', 'address', 'email', 'payment')
 
     def email_html(self, obj):
         return '<a href="mailto:{0}">{0}</a>'.format(obj.email)
