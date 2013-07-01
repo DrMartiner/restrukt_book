5a7d1c8b106578d5695c7a7af5417f913c5ab3d2
DrMartiner <DrMartiner@GMail.Com>
2013-07-01 15:53:51 +0700

    Upgrade pay2pay

--- apps/simple_page/urls.py
+++ apps/simple_page/urls.py
@@ -3,12 +3,8 @@
 from django.conf.urls import patterns, url
 from .views import HomePage
 from .views import MakeOrder
-from .views import OrderFail
-from .views import OrderSuccess
 
 urlpatterns = patterns('',
     url(r'^$', HomePage.as_view(), name='home_page'),
     url(r'^make-order/$', MakeOrder.as_view(), name='order_make'),
-    url(r'^fail/$', OrderFail.as_view(), name='order_fail'),
-    url(r'^success/$', OrderSuccess.as_view(), name='order_success'),
 )
--- apps/simple_page/views.py
+++ apps/simple_page/views.py
@@ -2,11 +2,9 @@
 
 import json
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView, FormView
 from .forms import OrderForm
 from constance import config
-from pay2pay.views import Confirm
 from pay2pay.models import Payment
 from .models import Video
 
@@ -48,19 +46,3 @@ class MakeOrder(FormView):
         }
 
         return HttpResponse(json.dumps(pay_data), status=201, mimetype='application/json')
-
-
-class OrderSuccess(TemplateView, Confirm):
-    template_name = 'simple_page/order_success.html'
-
-    def post(self, request, *args, **kwargs):
-        super(OrderSuccess, self).post(request, *args, **kwargs)
-        return super(OrderSuccess, self).get(request, *args, **kwargs)
-
-
-class OrderFail(TemplateView, Confirm):
-    template_name = 'simple_page/order_fail.html'
-
-    def post(self, request, *args, **kwargs):
-        super(OrderFail, self).post(request, *args, **kwargs)
-        return super(OrderFail, self).get(request, *args, **kwargs)
