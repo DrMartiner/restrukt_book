0e3822700440c759cb7ef0411c42d0d14658b283
DrMartiner <DrMartiner@GMail.Com>
2013-07-01 16:20:56 +0700

    Casual admin

--- apps/simple_page/admin.py
+++ apps/simple_page/admin.py
@@ -11,7 +11,7 @@ class VideoAdmin(admin.ModelAdmin):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('name', 'sent', 'address', 'email_html', 'created')
+    list_display = ('name', 'sent', 'status', 'address', 'email_html', 'created')
     list_filter = ('sent', 'created', 'payment__status')
     list_editable = ('sent',)
     search_fields = ('name', 'address')
@@ -22,6 +22,12 @@ class OrderAdmin(admin.ModelAdmin):
     def email_html(self, obj):
         return '<a href="mailto:{0}">{0}</a>'.format(obj.email)
     email_html.allow_tags = True
+    email_html.short_description = 'Email'
+
+    def status(self, obj):
+        return obj.payment.get_status_display()
+    status.allow_tags = True
+    status.short_description = 'Статус платежа'
 
 admin.site.register(Video, VideoAdmin)
 admin.site.register(Order, OrderAdmin)
